ed82b9decf40958d2e5fe78fbcdedab0e034175e
mystylal-ui <mikelovemrrr@gmail.com>
2026-03-10 20:13:20 +0300

    Initial Commit

---
+++ Navigation/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Mysty Mystylal on 10.03.2026.
+//
+
+import Foundation
+
+struct Post{
+    let title : String
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Mysty Mystylal on 10.03.2026.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Profile"
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
